9b85504eb6c4d7fa814b717aee2f1b8a7b8c6fc9
e_smirnov <5557192+jedi-philosopher@users.noreply.github.com>
2013-07-26 15:32:34 +0400

    Added artifact build and example run scripts

---
+++ frankenstein-examples/scripts/run_example.bat
@@ -0,0 +1,1 @@
+java -jar frankenstein-examples.jar -i resources/test_set/test_parts_set.json -o ./out -c 3
---
+++ frankenstein-examples/scripts/run_example.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar frankenstein-examples.jar -i ./resources/test_set/test_parts_set.json -o ./out -c 3
